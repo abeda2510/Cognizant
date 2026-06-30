d61f2022093d6e6770ac6181b3a5d9bde3f456ae
Abeda Begum <skabeda82@gmail.com>
2026-06-30 10:01:15 +0530

    Add files via upload

---
+++ Digital Nurture 5.0/Deepskilling/Week-2/EntityFrameworkCore8/Lab4_InsertInitialData/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 4 - Insert Initial Data");
+
+            Console.WriteLine("Category: Electronics");
+            Console.WriteLine("Category: Groceries");
+
+            Console.WriteLine("Product: Laptop - ₹75000");
+            Console.WriteLine("Product: Rice Bag - ₹1200");
+
+            Console.WriteLine("Initial Data Inserted Successfully.");
+        }
+    }
+}
