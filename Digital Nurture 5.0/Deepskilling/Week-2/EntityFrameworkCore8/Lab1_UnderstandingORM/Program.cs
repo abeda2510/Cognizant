eca3691919c8752691148bf5a101a31880d2ac9c
Abeda Begum <skabeda82@gmail.com>
2026-06-30 09:13:44 +0530

    Add files via upload

---
+++ Digital Nurture 5.0/Deepskilling/Week-2/EntityFrameworkCore8/Lab1_UnderstandingORM/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Retail Inventory System");
+            Console.WriteLine("Lab 1 - Understanding ORM with Entity Framework Core 8.0");
+            Console.WriteLine("Project Created Successfully.");
+        }
+    }
+}
