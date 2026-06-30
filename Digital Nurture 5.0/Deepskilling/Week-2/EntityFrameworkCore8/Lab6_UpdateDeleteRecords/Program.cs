754e70234385d96fa623d9e99efff43a22a8ee42
Abeda Begum <skabeda82@gmail.com>
2026-06-30 10:06:59 +0530

    Add files via upload

---
+++ Digital Nurture 5.0/Deepskilling/Week-2/EntityFrameworkCore8/Lab6_UpdateDeleteRecords/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 6 - Update and Delete");
+
+            Console.WriteLine("Laptop Price Updated to ₹70000");
+
+            Console.WriteLine("Rice Bag Deleted");
+
+            Console.WriteLine("Database Updated Successfully.");
+        }
+    }
+}
