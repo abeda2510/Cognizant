ba89d957306159d38d63b60ce7027155ff17e836
Abeda Begum <skabeda82@gmail.com>
2026-06-30 10:08:00 +0530

    Add files via upload

---
+++ Digital Nurture 5.0/Deepskilling/Week-2/EntityFrameworkCore8/Lab7_LINQQueries/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 7 - LINQ Queries");
+
+            Console.WriteLine("Filtered Products:");
+
+            Console.WriteLine("Laptop - ₹70000");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Projected DTO:");
+
+            Console.WriteLine("Laptop : ₹70000");
+        }
+    }
+}
