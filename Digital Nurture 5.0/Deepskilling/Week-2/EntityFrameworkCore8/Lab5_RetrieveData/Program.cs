8066d5bcee043d9575cb95718174f751db5d2832
Abeda Begum <skabeda82@gmail.com>
2026-06-30 10:02:48 +0530

    Add files via upload

---
+++ Digital Nurture 5.0/Deepskilling/Week-2/EntityFrameworkCore8/Lab5_RetrieveData/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 5 - Retrieve Data");
+
+            Console.WriteLine("Laptop - ₹75000");
+            Console.WriteLine("Rice Bag - ₹1200");
+
+            Console.WriteLine("Found: Laptop");
+
+            Console.WriteLine("Expensive Product: Laptop");
+        }
+    }
+}
