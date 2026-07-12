111f92993da7fe09933425135d877800b55ea5ed
Abeda Begum <skabeda82@gmail.com>
2026-07-12 11:46:41 +0530

    Add files via upload

---
+++ Digital Nurture 5.0/Deepskilling/Week-2/AspNetCoreWebAPI/WebApi_Handson2/Models/Employee.cs
@@ -0,0 +1,13 @@
+namespace WebApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Salary { get; set; }
+
+        public bool Permanent { get; set; }
+    }
+}
